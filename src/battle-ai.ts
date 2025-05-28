c4384a53d863c0f11791a4c028d3a61280c3735f
SweetCoffy <sweetcoffy@proton.me>
2025-05-28 12:09:36 -0400

    rework bot ai: episode 1.1

--- src/battle-ai.ts
+++ src/battle-ai.ts
@@ -7,6 +7,16 @@ export class BotAI {
     constructor(readonly battle: Battle, readonly player: Player) {
 
     }
+    getAllies() {
+        let p = this.player
+        let b = this.battle
+        return this.battle.players.filter(other => !b.isEnemy(p, other))
+    }
+    getEnemies() {
+        let p = this.player
+        let b = this.battle
+        return this.battle.players.filter(other => b.isEnemy(p, other))
+    }
     getAction(): AIAction {
         return {
             move: "bonk",
